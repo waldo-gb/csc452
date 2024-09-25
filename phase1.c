ca24f3688c0a78253ed58ebcefd00881741b714e
connoroneill704 <150087351+connoroneill704@users.noreply.github.com>
2024-09-25 10:04:32 -0700

    Update phase1.c

--- phase1.c
+++ phase1.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-/*#include <phase1.h>
+#include <phase1.h>
 #include <usloss.h>
-/*
-list of dead and alive childern
-child status get collected by parent in join
-need 6 different run queues
-need a pointer or  something for the table
-a process has a list of people waiting for it to die
-write init as if there are other phases running
-table[1].pid=1;
-table[1].......
-*/
-
+#include <stdlib.h>
 /*
  * Maximum number of processes. 
  */
@@ -49,26 +39,34 @@ typedef struct proc{
     struct proc *child;   
     struct proc *parent;
     USLOSS_Context context;
+    void *stack;
     int pid;
     int prio;
     int status;
     int state;
     char name[32];
-    int (*startFunc)(char*);
-    char *arg; 
+    int (*startFunc)(void*);
+    void *arg; 
     int exit_status;
 } proc;
-/****CONSTRUCTOR***kinda
-USLOSS_ContextInit(&table[i].context);*/
 struct proc table[MAXPROC];
 struct proc *queue[6][MAXPROC];
 int queue_len[6];
 int curr_pid=-1;
 struct proc *curr_pcb=NULL;
-int testcase_main(void){
+#ifndef EXTERNAL_TESTCASE_MAIN
+int testcase_main(){
     return testcase_main();
 }
-int spork(char *name, int (*startFunc)(char*), char *arg, int stackSize, int priority) {
+#endif
+int testcase_bounce(void*){
+    return testcase_main();
+}
+void trampoline() {
+    int curr=getpid();
+    table[curr-1].startFunc(table[curr-1].arg);
+}
+int spork(char *name, int (*startFunc)(void*), void *arg, int stackSize, int priority) {
     if(stackSize<USLOSS_MIN_STACK){
         return -2;
     }
@@ -95,10 +93,12 @@ int spork(char *name, int (*startFunc)(char*), char *arg, int stackSize, int pri
                 curr->younger=new;
                 new->older=curr;
             }
+            new->stack=malloc(USLOSS_MIN_STACK);
+            USLOSS_ContextInit(&new->context,new->stack,USLOSS_MIN_STACK,NULL,trampoline);
             int p=new->prio-1;
             queue[p][queue_len[p]++]=new;
+            return curr_pid;
         }
-        return curr_pid;
     }
     return -1;
 }
@@ -135,7 +135,7 @@ int join(int *status){
     return -2;
 }
 
-void quit_phase_1a(int status, int switchToPid) {
+__attribute__((noreturn)) void quit_phase_1a(int status, int switchToPid) {
     proc *curr=&table[curr_pid-1];
     curr->exit_status=status;
     curr->state=-2;
@@ -148,19 +148,19 @@ void quit_phase_1a(int status, int switchToPid) {
             queue_len[p]--;
             break;
         }
-    }
-    ULOSS_ContextSwitch(switchToPid);
+    }   
+    USLOSS_ContextSwitch(&table[curr_pid-1].context,&table[switchToPid-1].context);
 }
 
 int  getpid(void){
     return curr_pid;
 }
 
-extern void dumpProcesses(void){
+void dumpProcesses(void){
     printf("Dump Process");
     for(int i=0; i<MAXPROC; i++){
         if(table[i].pid!=-1){
-            printf("%5d %5s %5d %5d %5s\n", 
+            printf("%5d %5s %5d %5d %5d\n", 
             table[i].pid, 
             table[i].name,
             table[i].prio, 
@@ -176,7 +176,6 @@ void TEMP_switchTo(int pid){
 void phase1_init(void) {
     memset(table,0,sizeof(table));
     memset(queue_len,0,sizeof(table));
-    int val;
     for(int i=0; i<=MAXPROC; i++){
         table[i].pid=0;
         table[i].status=0;
@@ -184,20 +183,22 @@ void phase1_init(void) {
     curr_pid=1;
     strcpy(table[1].name, "init");
     table[1].pid=1;
-    table[1].status=0;
+    table[1].status=0;  
     table[1].prio=6;
     table[1].state=RUNNING;
     curr_pcb=&table[1];
-    int testcase=spork("testcase_main",testcase_main, NULL, USLOSS_MIN_STACK, 3);
-    ULOSS_ContextSwitch(testcase);
+    int testcase=spork("testcase_main",testcase_bounce, NULL, USLOSS_MIN_STACK, 3);
+    USLOSS_ContextSwitch(&table[1].context,&table[testcase-1].context);
     printf("testcase_main returned...haulting");
-    USLOSS_HAULT(0);
+    USLOSS_Halt(0);
+    int i=2;
     while(1){
-        int join_val=join(&status);
+        int join_val=join(&table[i].status);
         if(join_val==-2){
             printf("join_val=-2");
-            USLOSS_HAULT(0);
+            USLOSS_Halt(0);
             break;
         }
+        i++;
     }
 }
