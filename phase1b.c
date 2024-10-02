aa88a29bb97cddd48147a75fbf66d228ebe75706
connoroneill704 <150087351+connoroneill704@users.noreply.github.com>
2024-10-01 22:52:02 -0700

    Update phase1b.c

--- phase1b.c
+++ phase1b.c
@@ -1,9 +1,6 @@
 /*
  * These are the definitions for phase1 of the project (the kernel).
  */
-
-#ifndef _PHASE1_H
-#define _PHASE1_H
 #include <stdio.h>
 #include <string.h>
 #include <phase1.h>
@@ -42,6 +39,7 @@
 /* 
  * These functions must be provided by Phase 1.
  */
+
 typedef struct proc{
     struct proc *older;
     struct proc *younger; 
@@ -58,19 +56,17 @@ typedef struct proc{
     void *arg; 
     int exit_status;
 } proc;
+
 struct proc table[MAXPROC];
 struct proc *queue[6][MAXPROC];
 int queue_len[6];
 int curr_pid=-1;
 struct proc *curr_pcb=NULL;
-// #ifndef EXTERNAL_TESTCASE_MAIN
-// int testcase_main(){
-//     return testcase_main();
-// }
-// #endif
+
 int testcase_bounce(void*){
     return testcase_main();
 }
+
 void trampoline() {
     int curr=getpid();
     for(int i = curr; (i<=MAXPROC && table[i].pid!=0); i++){
@@ -81,31 +77,9 @@ void trampoline() {
         USLOSS_PsrSet(psr | USLOSS_PSR_CURRENT_INT);
     }
     table[curr-1].startFunc(table[curr-1].arg);
-    quit(table[curr_pid - 1]->status);
+    quit(table[curr_pid - 1].status);
 }
-void dispatcher(void) {
-    int end=0;
-    for (int j=0;j<6;j++) {
-        for (int i=0;i<queue_len[p];i++) {
-            if (queue[j][i]->state==0) {
-                TEMP_switchTo(queue[j][i]->pid);
-                end=1;
-                break;
-            }
-        }
-        if (end==1){
-            break;
-        }
-    }
-    if (end==0) {
-        USLOSS_Console("Error\n");
-        USLOSS_Halt(1);
-    }
-}
-int currentTime(void) {
-    time_t curr=time(NULL);
-    return (int)curr;
-}
+
 int spork(char *name, int (*startFunc)(void*), void *arg, int stackSize, int priority) {
     int psr=USLOSS_PsrGet();
     if (!(psr & USLOSS_PSR_CURRENT_MODE)) {
@@ -116,9 +90,16 @@ int spork(char *name, int (*startFunc)(void*), void *arg, int stackSize, int pri
         USLOSS_Console("ERROR: It looks like you are running testcase_main() with interrupts disabled, instead of enabled. Fix that!\n");
         USLOSS_Halt(1);
     }
+    if ((psr & USLOSS_PSR_CURRENT_INT)!=0) {
+        USLOSS_PsrSet(psr & ~USLOSS_PSR_CURRENT_INT);
+    }
     if(stackSize<USLOSS_MIN_STACK){
         USLOSS_PsrSet(psr);
         return -2;
+    }
+    if (priority<1 || priority>6) {
+        USLOSS_PsrSet(psr);
+        return -1;
     }
     for(int i=1; i<=MAXPROC; i++){
         if(table[i].pid==0){
@@ -207,10 +188,6 @@ int join(int *status){
 
 __attribute__((noreturn)) void quit(int status) {
     int psr=USLOSS_PsrGet();
-    if (curr_pid<1 || curr_pid>MAXPROC || table[curr_pid-1].pid==0) {
-        USLOSS_Console("Error: Invalid process attempting to quit (PID: %d).\n",curr_pid);
-        USLOSS_Halt(1);
-    }
     proc *curr=&table[curr_pid-1];
     if (!(psr & USLOSS_PSR_CURRENT_MODE)) {
         USLOSS_Console("ERROR: Someone attempted to call quit_phase_1a while in user mode!\n");
@@ -263,6 +240,7 @@ __attribute__((noreturn)) void quit(int status) {
     }
     dispatcher();
 }
+
 void zap(int pid) {
     if (pid==curr_pid || pid==1 || table[pid-1].pid==0 || table[pid-1].state==DEAD) {
         USLOSS_Console("Error: Cannot zap PID %d.\n", pid);
@@ -275,13 +253,16 @@ void zap(int pid) {
     }
     curr_pcb->state=READY;
 }
+
 int  getpid(void){
     return curr_pid;
 }
+
 void blockMe(void) {
     table[curr_pid - 1].state=BLOCKED;
     dispatcher();
 }
+
 int unblockProc(int pid) {
     if (pid<1 || pid>MAXPROC || table[pid-1].state!=BLOCKED || table[pid-1].status<=10) {
         return -2;
@@ -292,12 +273,13 @@ int unblockProc(int pid) {
     dispatcher();
     return 0;
 }
+
 void dumpProcesses() {
-    USLOSS_Console("**************** Calling dumpProcesses() *******************\n");
-    USLOSS_Console("- PID  PPID  NAME              PRIORITY  STATE\n");
+    USLOSS_Console(" PID  PPID  NAME              PRIORITY  STATE\n");
     for (int i=0; i<MAXPROC;i++) {
         if (table[i].pid==0) continue;
         char *str;
+        int num=table[i].parent!=NULL ? table[i].parent->pid:0;
         switch (table[i].state) {
             case DEAD:
                 str="Terminated";
@@ -314,7 +296,7 @@ void dumpProcesses() {
             default:
                 str="SHOULDN'T BE HERE";
         }
-        USLOSS_Console("- %3d  %4d  %-15s  %8d  %s\n", 
+        USLOSS_Console(" %3d  %4d  %-15s  %8d  %s\n", 
             table[i].pid, 
             table[i].parent!=NULL ? table[i].parent->pid:0,
             table[i].name, 
@@ -322,19 +304,49 @@ void dumpProcesses() {
             str);
     }
 }
-void TEMP_switchTo(int pid){
+
+void swapTo(int pid){
     //USLOSS_Console("Name: %s, PID: %d\n", &table[curr_pid-1].name, table[curr_pid-1].pid);
     //USLOSS_Console("Name: %s, PID: %d\n", &table[pid-1].name, table[pid-1].pid);
     int psr=USLOSS_PsrGet();
     if ((psr & USLOSS_PSR_CURRENT_INT)!=0) {
         USLOSS_PsrSet(psr & ~USLOSS_PSR_CURRENT_INT);
     }
+    if(table[curr_pid - 1].state!=-2) {
+        table[curr_pid - 1].state=0;
+    }
     int old = curr_pid;
     curr_pid=pid;
     curr_pcb=&table[pid-1];
+    table[pid - 1].state=1;
     USLOSS_ContextSwitch(&table[old-1].context,&table[pid-1].context);
     USLOSS_PsrSet(psr | USLOSS_PSR_CURRENT_INT);
 }
+
+void dispatcher(void) {
+    int end=0;
+    for (int j=0;j<6;j++) {
+        for (int i=0;i<queue_len[j];i++) {
+            if (queue[j][i]->state==0) {
+                swapTo(queue[j][i]->pid);
+                end=1;
+                break;
+            }
+        }
+        if (end==1){
+            break;
+        }
+    }
+    if (end==0) {
+        USLOSS_Console("Error\n");
+        USLOSS_Halt(1);
+    }
+}
+
+//int currentTime(void) {
+    //time_t curr=time(NULL);
+    //return (int)curr;
+//}
 void phase1_init(void) {
     memset(table,0,sizeof(table));
     memset(queue_len,0,sizeof(queue_len));
@@ -353,11 +365,10 @@ void phase1_init(void) {
     table[0].child=NULL;
     table[0].older=NULL;
     table[0].younger=NULL;
-    USLOSS_ContextInit(table[0].context,table[0].stack, USLOSS_MIN_STACK, NULL, trampoline);
-    int priority=table[0]->prio-1;
-    queue[priority][queue_len[priority]++]=table[0];
-    USLOSS_Console("Phase 1A TEMPORARY HACK: init() manually switching to PID 1.\n");
-    TEMP_switchTo(1);
+    USLOSS_ContextInit(&table[0].context,table[0].stack, USLOSS_MIN_STACK, NULL, trampoline);
+    int priority=table[0].prio-1;
+    queue[priority][queue_len[priority]++]=&table[0];
+    swapTo(1);
     phase2_start_service_processes();
     phase3_start_service_processes();
     phase4_start_service_processes();
@@ -370,16 +381,10 @@ void phase1_init(void) {
         USLOSS_Halt(1);
     }
     int testcase=spork("testcase_main",testcase_bounce, NULL, USLOSS_MIN_STACK, 3);
-    USLOSS_Console("Phase 1A TEMPORARY HACK: init() manually switching to testcase_main() after using spork() to create it.\n");
-    TEMP_switchTo(testcase);
-    USLOSS_Console("Phase 1A TEMPORARY HACK: testcase_main() returned, simulation will now halt.\n");
-    //printf("testcase_main returned...haulting");
-    USLOSS_Halt(0);
     int i=1;
     while(1){
         int join_val=join(&table[i].status);
         if(join_val==-2){
-            printf("join_val=-2");
             USLOSS_Halt(0);
             break;
         }
